f1b5256d6087c38fd52888ec28182b5c6533bfa2
Mikael <mikaelkellerfox@gmail.com>
2021-09-27 15:14:52 -0300

    Primeiro

---
+++ Inicio.js
@@ -0,0 +1,15 @@
+console.log("Hello Word!!!!!!");
+const Site = "www.google.com";
+
+let url = "www.yahoo.com"
+var mostrarsite = false;
+
+if(mostrarsite){
+    url = "Você também pode usar o Yahoo !!"
+    console.log(Site,url)
+    
+}else{
+    console.log("Site fora de Ar usse o ",url)
+}
+
+
